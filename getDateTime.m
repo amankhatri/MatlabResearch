c717c54b0d249da1c17e377013fe8251a29ee2fc
amankhatri <aman_khatri5b5@yahoo.com>
2014-09-26 14:03:35 -0500

    NO MORE UPLOADS

--- getDateTime.m
+++
@@ -1,8 +0,0 @@
-function [ dates times dates_times ] = getDateTime(rawdata)
-%getDateTime Summary of this function goes here
-%   it gets Date and time from rawdatafile
-dates = rawdata(2:end,5);
-times = rawdata(2:end,6);
-dates_times = [dates times];
-end
-
--- getRawData.m
+++
@@ -1,7 +0,0 @@
-function [ rawdata ] = getRawData(filename)
-%getRawDat Summary of this function goes here
-%   It reads excel files
-[numeric,text,rawdata] = xlsread(filename);
-
-end
-
